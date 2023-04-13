34592d7ee0f32f7d0bae861fcfae8045b1966a14
Georgiy Sagitov <georgmc@yandex.ru>
2023-04-13 23:45:15 +0300

    Added animation

--- app/src/main/java/ru/netology/statsview2/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview2/MainActivity.kt
@@ -2,6 +2,9 @@ package ru.netology.statsview2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import ru.netology.statsview.ui.StatsView
 
 
@@ -9,6 +12,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<StatsView>(R.id.statsView).data = listOf(500F, 500F, 500F, 500F)
+        val view = findViewById<StatsView>(R.id.statsView)
+        view.data = listOf(500F, 500F, 500F, 500F)
+
     }
+
 }
